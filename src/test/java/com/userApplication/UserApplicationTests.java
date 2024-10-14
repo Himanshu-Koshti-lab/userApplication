9f2fd761c0383bd7f94a0a55999b5a07a372b622
Himanshu Koshti <himfeedback74@gmail.com>
2024-10-14 22:39:17 +0530

    Setup

---
+++ src/test/java/com/userApplication/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
